b7373b7abf2432db521150db0c7b44f6688969d6
Cesar Esparza <cesparza@wd40.com>
2019-08-08 14:54:20 -0700

    Updated footer and project screenshot

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -4,8 +4,11 @@ const Footer = () => (
   <div style={Styles.footer}>
     <hr/>
     <Link href='/'>
-      <a style={Styles.a}>cesaresparza.dev</a>
+      <a style={Styles.a}>CesarEsparza.dev</a>
     </Link>
+      <span> - 
+        <Link href='https://github.com/cesaresparzadev/cesaresparza.dev'><a> This site was built on React/Next</a></Link>
+      </span>
   </div>
 )
 
